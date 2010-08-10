1e4ebb3e5ca1467dd5239d3344c53ddcc473d70e
Moritz Winterberg <winterberg@sipgate.de>
2010-08-10 13:06:42 +0200

    add refresh functionality to menu button in EventList activity

--- src/com/sipgate/ui/EventListActivity.java
+++ src/com/sipgate/ui/EventListActivity.java
@@ -317,7 +317,7 @@ public class EventListActivity extends Activity {
 		return dateformatterPretty.format(d);
 	}
 
-	protected void getEvents() {
+	public void getEvents() {
 		try {
 			if (serviceBinding != null) {
 				List<Event> events = serviceBinding.getEvents();
